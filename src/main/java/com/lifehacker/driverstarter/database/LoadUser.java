f96922ac0ce04e1ec0b2ded6b1849c5e440e482b
mothusi <molorane.mothusi@gmailcom>
2022-06-10 18:13:54 +0200

    java

--- src/main/java/com/lifehacker/driverstarter/database/LoadUser.java
+++ src/main/java/com/lifehacker/driverstarter/database/LoadUser.java
@@ -29,7 +29,11 @@ public class LoadUser {
 
     private void createTable(final JdbcTemplate jdbcTemplate) {
         logger.info("Attempting to create a table..");
-        final String SQL = "CREATE TABLE IF NOT EXISTS gdpuser(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, age INT NOT NULL)";
+        final String SQL = """
+                CREATE TABLE IF NOT EXISTS gdpuser(
+                id INT AUTO_INCREMENT PRIMARY KEY, 
+                name VARCHAR(255) NOT NULL, 
+                age INT NOT NULL)""";
         jdbcTemplate.execute(SQL);
         logger.info("Creating a table complete..");
     }
